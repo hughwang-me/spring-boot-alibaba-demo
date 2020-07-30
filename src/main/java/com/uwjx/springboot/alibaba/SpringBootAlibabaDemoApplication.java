92f6031aab8a8aa246fdf7df8d3c4097dff10fa9
Hugh Wang <18501667737@163.com>
2020-07-30 11:40:46 +0800

    init

---
+++ src/main/java/com/uwjx/springboot/alibaba/SpringBootAlibabaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.springboot.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAlibabaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAlibabaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/springboot/alibaba/SpringBootAlibabaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwjx.springboot.alibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAlibabaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
